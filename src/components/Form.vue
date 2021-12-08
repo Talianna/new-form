aca717e47ee8906caf3876a80ee3e1e879edf9bd
Talianna <nat_joe@mail.ru>
2021-12-08 16:31:55 +0300

    groups styles

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,27 +1,35 @@
 <template>
   <div>
-    <p>form</p>
     <form class="form" @submit.prevent="onSubmit">
 
-      <input class="form-input" type="text">
-      <input class="form-input" type="email">
-      <input class="form-input" type="tel">
-      <input class="form-input" type="url">
-      <input class="form-input" type="number">
-      <input class="form-input" type="date">
+      <div class="input-group">
+        <input class="form-input" type="text">
+        <input class="form-input" type="email">
+      </div>
 
-      <select class="form-input">
-        <option>5</option>
-        <option>4</option>
-        <option>3</option>
-        <option>2</option>
-        <option>1</option>
-      </select>
+      <div class="input-group">
+        <input class="form-input" type="tel">
+        <input class="form-input" type="url">
+      </div>
 
-      <textarea class="form-input" name="" id="" cols="30" rows="10"></textarea>
+      <div class="input-group">
+        <input class="form-input" type="number">
+        <input class="form-input" type="date">
+      </div>
+
+      <div class="input-group">
+        <select class="form-input">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </div>
+
+      <textarea class="form-input form-textarea" name="" id="" cols="30" rows="10"></textarea>
 
       <button type="submit" class="form-btn">Submit</button>
-
     </form>
   </div>
 </template>
@@ -30,5 +38,5 @@
   export default {
     name: 'Form'
   }
-
+  
 </script>
