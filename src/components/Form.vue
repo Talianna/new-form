5f38f448040f490f790d3ca3a4f965c91e691d0b
Talianna <nat_joe@mail.ru>
2021-12-08 16:19:55 +0300

    styles

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,14 +3,14 @@
     <p>form</p>
     <form class="form" @submit.prevent="onSubmit">
 
-      <input type="text">
-      <input type="email">
-      <input type="tel">
-      <input type="url">
-      <input type="number">
-      <input type="date">
+      <input class="form-input" type="text">
+      <input class="form-input" type="email">
+      <input class="form-input" type="tel">
+      <input class="form-input" type="url">
+      <input class="form-input" type="number">
+      <input class="form-input" type="date">
 
-      <select>
+      <select class="form-input">
         <option>5</option>
         <option>4</option>
         <option>3</option>
@@ -18,13 +18,11 @@
         <option>1</option>
       </select>
 
-      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <textarea class="form-input" name="" id="" cols="30" rows="10"></textarea>
 
-      <button></button>
-
+      <button type="submit" class="form-btn">Submit</button>
 
     </form>
-
   </div>
 </template>
 
@@ -34,7 +32,3 @@
   }
 
 </script>
-
-<style scoped>
-
-</style>
