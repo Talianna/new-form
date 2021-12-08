ca881afebfdf2777a93160e281f9ec2db0626f8b
Talianna <nat_joe@mail.ru>
2021-12-08 15:42:29 +0300

    start form

---
+++ src/components/Form.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p>form</p>
+    <form class="form" @submit.prevent="onSubmit">
+
+      <input type="text">
+      <input type="email">
+      <input type="tel">
+      <input type="url">
+      <input type="number">
+      <input type="date">
+
+      <select>
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+      </select>
+
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+
+      <button></button>
+
+
+    </form>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Form'
+  }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/NewForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>The New Form</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore ea reiciendis, rerum dolorum at aspernatur non quam corporis rem assumenda.</p>
+        
+        <Form />
+
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+</template>
+
+<script>
+import Form from './Form.vue'
+
+
+export default {
+  name: 'NewForm',
+
+  components: {
+      Form
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
